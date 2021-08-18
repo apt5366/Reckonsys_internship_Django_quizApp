931317ec308c1eebb9a2bc86f4d983f2650202f1
*apt5366* <*apt5366@psu.edu*>
2021-08-18 08:34:38 +0530

    Score implementation working properly

--- quiz/views.py
+++ quiz/views.py
@@ -88,27 +88,50 @@ def checkAns(request, question_id, student_id): #checks whether the answer attem
         
 
         #answer indicates whether attempt was correct or not 
-        answer = 1 if (selected_choice==question.right_choice) else 0 
+        answer = 1 if (selected_choice.choice_seq==question.right_choice) else 0 
 
         if(answer==1): # answer correct
-            if(question.q_attempted==0): # Qn was not previosuly attempted
+            if(question.q_attempted==0): # 0 -- Qn was not previosuly attempted
                 student.student_score =student.student_score + 10 #Student gets 10 points
                 student.save()
-            #No else cauz it was prev attempted and it was correct so No change in score
+            else: # Qn was attempted previously
+                if(question.q_attempted!=question.right_choice): # Qn was attempted incorrectly previously
+                    student.student_score =student.student_score+10 #Student gains 10 points as he/she has modified their answer and got it right this time
+                    student.save()
+                
+                # No else as Qn was attempted correctly last time as well, so we dont make any change
         else: # Answer wrong in this attempt
-            if(question.q_attempted==1): # Qn WAS previously attempted
+            if(question.q_attempted==question.right_choice): # Qn WAS previously attempted correctly
                 student.student_score =student.student_score-10 #Student looses 10 points as he/she has modified their answer
                 student.save()
             # No else cauz it was not prev attempted and it isnt correct now, so No change in score
         
-        question.q_attempted = 1 #indicates that questions has been attempted once so score should be incr or decr accordingly
+        question.q_attempted = selected_choice.choice_seq #indicates that questions has been attempted once so score should be incr or decr accordingly
         question.save()
+
+
+
+
+
+        # if(answer==1): # answer correct
+        #     if(question.q_attempted==0): # Qn was not previosuly attempted
+        #         student.student_score =student.student_score + 10 #Student gets 10 points
+        #         student.save()
+        #     #No else cauz it was prev attempted and it was correct so No change in score
+        # else: # Answer wrong in this attempt
+        #     if(question.q_attempted==1): # Qn WAS previously attempted
+        #         student.student_score =student.student_score-10 #Student looses 10 points as he/she has modified their answer
+        #         student.save()
+        #     # No else cauz it was not prev attempted and it isnt correct now, so No change in score
+        
+        # question.q_attempted = 1 #indicates that questions has been attempted once so score should be incr or decr accordingly
+        # question.save()
 
         return HttpResponseRedirect(reverse('quiz:questions', args=(student.id,)))
         # return HttpResponseRedirect(reverse('quiz:index', args=(question.id,)))
 
 def submit(request, student_id):
-    
+
     # question = get_object_or_404(Question, pk=question_id)
 
     student=get_object_or_404(Student,pk=student_id)
